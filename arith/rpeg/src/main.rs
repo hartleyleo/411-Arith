c1f377d2bc6ce3c92a5f78ca53e5117ea1b634ff
hartleyleo <hartleyleo2001@gmail.com>
2023-11-06 10:38:17 -0500

    Restructure format
    Formatting was messed up from my misinterpretation of the code, should be fixed now

--- arith/rpeg/src/main.rs
+++ arith/rpeg/src/main.rs
@@ -1,7 +1,19 @@
 use csc411_image;
 use csc411_arith;
-
+use std::env;
+use rpeg::codec::{compress, decompress};
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let argnum = args.len();
+    assert!(argnum == 2 || argnum == 3);
+    let filename = args.iter().nth(2).unwrap();
+    match args[1].as_str() {
+        "-c" => compress(Some(filename)),
+        "-d" => decompress(Some(filename)),
+        _ => {
+            eprintln!("Usage: rpeg -d [filename]\nrpeg -c [filename]")
+        }
+    }
 }
+
--- bitpack/src/codec.rs
+++
@@ -1,9 +0,0 @@
-use csc411_image;
-
-pub fn compress(filename: Option<&str>) {
-    todo!();
-}
-
-pub fn decompress(filename: Option<&str>) {
-    todo!();
-}
