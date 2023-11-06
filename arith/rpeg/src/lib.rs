6923c1146790bb7635eb91b4c405e45abe2e9659
hartleyleo <hartleyleo2001@gmail.com>
2023-11-06 11:16:33 -0500

    Update lib.rs
    Committing work

--- arith/rpeg/src/lib.rs
+++ arith/rpeg/src/lib.rs
@@ -1,4 +1,32 @@
-pub fn prepare_ppm() -> todo!() {
+use csc411_image::{RgbImage, Rgb};
+
+pub fn prepare_ppm(image: &RgbImage) -> todo!() {
+
+    // Trim edges if need be
+    if image.width % 2 != 0 {
+        let width = image.width -= 1;
+    }
+
+    if image.height % 2 != 0 {
+        let height = image.height -= 1;
+    }
+
+    // Store into Array2
+
+    // Create new Array2
+    let mut temp_vec = Vec<Rgb>;
+    // Loop through each index
+    // Set each new index to the value of the original images index
+
+    for i in 0..width {
+        for j in 0..height {
+            temp_vec.push(image.pixels[]);
+        }
+    }
+
+    // Return Array2
+    let prepared_image = Array2::new(val: T, width, height);
+    return prepared_image;
 
 }
 
