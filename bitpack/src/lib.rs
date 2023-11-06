b21466d816a79a698e985a1a65f931fef97ade03
hartleyleo <78177307+hartleyleo@users.noreply.github.com>
2023-11-06 10:25:31 -0500

    Create Bitpack
    Signed-off-by: hartleyleo <78177307+hartleyleo@users.noreply.github.com>

---
+++ bitpack/src/lib.rs
@@ -0,0 +1,31 @@
+pub mod bitpack;
+
+pub mod codec; // huh ??
+
+pub fn prepare_ppm() -> todo!() {
+
+}
+
+pub fn convert_to_rgb() -> todo!() {
+
+}
+
+pub fn convert_to_component_video() -> todo!() {
+
+}
+
+pub fn write_to_standard_output() -> todo!() {
+
+}
+
+pub fn convert_to_four_bit() -> todo!() {
+
+}
+
+pub fn discrete_cosine_transfer() -> todo!() {
+
+}
+
+pub fn inverse_discrete_cosine_transfer() -> todo!() {
+
+}
