24f0f79771241420dfc7e9d64af15f2c6e7d5b19
hartleyleo <hartleyleo2001@gmail.com>
2023-11-07 23:35:41 -0500

    component video conversion
    component video is now working

--- arith/rpeg/src/codec.rs
+++ arith/rpeg/src/codec.rs
@@ -1,22 +1,24 @@
 use csc411_image;
 use csc411_rpegio;
 use csc411_image::{Read, RgbImage};
-use crate::compress_decompress::{prepare_ppm};
+use crate::compress_decompress::{prepare_ppm, convert_to_rgb_float, convert_to_component_video};
 // use bitpack::bitpack::{newu, news};
 // use csc411_rpegio::{output_rpeg_data, read_in_rpeg_data};
 // use csc411_image::Write;
 
 pub fn compress(filename: Option<&str>) {
 
+    // Load in image
     let image = RgbImage::read(Some(filename.unwrap())).unwrap();
 
-    let rgb_float_image = prepare_ppm(&image);
+    // Load image into a Vec<Rgb>
+    let rgb_image = prepare_ppm(&image);
 
-    for i in 0..rgb_float_image.len() {
-        print!("{}", i);
-    }
+    // Translate the rgb vec into a vec with floating points for the rgb values
+    let rgb_float_image = convert_to_rgb_float(&rgb_image, image.denominator);
 
-    
+    // Translate the rgb float image into a vec of custom struct: Ypbpr
+    let component_video_image = convert_to_component_video(&rgb_float_image);
 
 }
 
--- arith/rpeg/src/compress_decompress.rs
+++ arith/rpeg/src/compress_decompress.rs
@@ -5,6 +5,20 @@ use array2::Array2;
 // Documenatation:
 // RgbImage: https://docs.rs/csc411_image/latest/csc411_image/struct.RgbImage.html
 
+#[derive(Clone, Debug)]
+pub struct RGBFloat {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
+
+#[derive(Clone, Debug)]
+pub struct Ypbpr {
+    pub y: f32,
+    pub pb: f32,
+    pub pr: f32,
+}
+
 // Function that reads in an RgbImage, trims off either the last row and/or column
 // to make the image evenly dimensioned, then loads the information into an Array2
 // of rgb values 
@@ -22,27 +36,65 @@ pub fn prepare_ppm(image: &RgbImage) -> Vec<csc411_image::Rgb> {
         height -= 1;
     }
 
-    // Store into Array2
     let mut pixel_data: Vec<Rgb> = vec![Rgb{red: 0, green: 0, blue: 0}; (width * height) as usize];
 
+    // Looks through each pixel (rgb value) in the image, and pushes each pixel into 
+    // a new vec for later manipulation
     for i in 0..height {
         for j in 0..width {
-            pixel_data[(width as usize * i as usize) + j as usize] = image.pixels[(image.width as usize * i as usize) + j as usize].clone();
+            pixel_data.push(image.pixels[(image.width as usize * i as usize) + j as usize].clone());
         }
     }
 
-    // Return Array2
     return pixel_data;
-
 }
 
-// pub fn convert_to_rgb() -> todo!() {
+pub fn convert_to_rgb_float(rbg_vec: &Vec<csc411_image::Rgb>, denominator: u16) -> Vec<RGBFloat> {
 
-// }
+    let mut rgb_float_image: Vec<RGBFloat> = vec![RGBFloat { r: 0.0, g: 0.0, b: 0.0 }; rbg_vec.len()];
 
-// pub fn convert_to_component_video() -> todo!() {
+    // For each pixel in the vec, push a copy of it to this new float image.
+    // To transform to a floating-point representation of RGB in respect to the 
+    // ppm format and its denominator, we divide each pixel's rgb values by
+    // said denominator and create a new floating point 'rgb' struct value
+    rgb_float_image = rbg_vec.iter()
+        .map(|el| RGBFloat {
+            r: el.red as f32 / denominator as f32,
+            g: el.green as f32 / denominator as f32,
+            b: el.blue as f32 / denominator as f32,
+        })
+        .collect();
 
-// }
+    // working for loop implementation:
+    // for el in rbg_vec.iter() {
+    //     rgb_float_image.push(RGBFloat { r: el.red as f32 / denominator as f32, g: el.green as f32 / denominator as f32, b: el.blue as f32 / denominator as f32});
+    // }
+
+    return rgb_float_image;
+}
+
+pub fn convert_to_component_video(rbg_float_vec: &Vec<RGBFloat>) -> Vec<Ypbpr> {
+
+    let mut component_video_per_pixel: Vec<Ypbpr> = vec![Ypbpr { y: 0.0, pb: 0.0, pr: 0.0 }; rbg_float_vec.len()];
+
+    // Based off of the formulas provided in the assignment description:
+    // y = 0.299 * r + 0.587 * g + 0.114 * b;
+    // pb = -0.168736 * r - 0.331264 * g + 0.5 * b;
+    // pr = 0.5 * r - 0.418688 * g - 0.081312 * b;
+    // ---------------------------------------------------------------------
+    // This block below takes each element in the rgb_float_vec vector and 
+    // maps its values to the Ypbpr data struct after running it through the
+    // appropriate conversion calculations
+    component_video_per_pixel = rbg_float_vec.iter()
+        .map(|el| Ypbpr {
+            y: (0.299 * el.r) + (0.587 * el.g) + (0.114 * el.b) as f32, 
+            pb: (-0.168736 * el.r) - (0.331264 * el.g) + (0.5 * el.b) as f32,
+            pr: (0.5 * el.r) - (0.418688 * el.g) - (0.081312 * el.b) as f32,
+        })
+        .collect();
+
+    return component_video_per_pixel;
+}
 
 // pub fn write_to_standard_output() -> todo!() {
 
